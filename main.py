947f66d9ccaa2a2606b18f94bf3142c51ca0365f
Vaibhavv1703 <vaibhavvsingh1703@gmail.com>
2025-04-30 23:29:01 +0530

    Fetch rockyou.pkl

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import re
+import os
 import pickle
+import urllib.request
 
-with open('rockyou.pkl', 'rb') as f:
+PICKLE_PATH = 'rockyou.pkl'
+REMOTE_URL = 'https://www.dropbox.com/scl/fi/weorggtspmqpd0hkp0eti/rockyou.pkl?rlkey=obyodpark2be1nlwzqbscn60h&st=n5bfbze7&dl=1'
+
+def download_pickle_file():
+    print(f"{PICKLE_PATH} not found. Downloading from {REMOTE_URL}...")
+    try:
+        urllib.request.urlretrieve(REMOTE_URL, PICKLE_PATH)
+        print("Download complete.")
+    except Exception as e:
+        print("Failed to download the file:", e)
+        exit(1)
+
+if not os.path.exists(PICKLE_PATH):
+    download_pickle_file()
+
+with open(PICKLE_PATH, 'rb') as f:
     common_passwords = pickle.load(f)
 
 def check_password_strength(password):
